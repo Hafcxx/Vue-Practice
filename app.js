feff9b7f68b636dd267540042259aca89251ea3b
Claudio Aninir <claudio.aninir01@gmail.com>
2022-11-24 11:37:07 -0300

    frutas llamadas a la lista como objetos, con span if

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const { createApp } = Vue
     data() {
         return {
             message: 'Hello Vue!',
-            frutas: ['Manzana', 'Pera', 'Platano'],
+            frutas: [
+                {nombre:'Manzana', cantidad: 10},
+                {nombre:'Pera', cantidad: 0 },
+                {nombre:'Platano', cantidad: 10} ],
             nuevaFruta : ''
         }
     },
     methods: {
         agregarFruta (){
-            this.frutas.push(this.nuevaFruta);
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad: 0
+            });
             this.nuevaFruta = '';
         }
-        
-
     }
 }).mount('#app')
