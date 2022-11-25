637f49c44ca25006e88eea5a353913783d445e22
Claudio Aniñir <claudio.aninir01@gmail.com>
2022-11-25 11:33:59 -0300

    trabajo con vuex en el cli

---
+++ proyecto-2/src/components/Lista.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li v-for="(fruta, index) of frutas" :key="fruta.id" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{fruta.nombre}}
+                <span class="badge badge-primary badge-pill">{{fruta.cantidad}}</span>
+            </li>
+        </ul>
+        <button class="btn m-2" @click="reiniciar">Reiniciar</button>
+    </div>
+</template>
+
+<script>
+
+import { mapState, mapMutations } from 'vuex';
+
+    export default{
+        name: 'Lista',
+        computed:{
+            ...mapState(['frutas'])
+        },
+        methods: {
+            ...mapMutations(['aumentar', 'reiniciar'])
+        }
+    }
+</script>
